b2d044d2144ea76a841b15df28c72d7dce5b30e6
Azeem <azeemmomin369@gmail.com>
2023-09-04 00:14:19 +0530

    added home page "our feature" and "our product"

--- html/script.js
+++ html/script.js
@@ -4,6 +4,9 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>
 {
     searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
 
 // this is shopping cart action
@@ -12,4 +15,42 @@ let shoppingCart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick = () =>
 {
     shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
 }
+
+// login form
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>
+{
+     loginForm.classList.toggle('active');
+     searchForm.classList.remove('active');
+     shoppingCart.classList.remove('active');
+     navbar.classList.remove('active');
+
+}
+
+// menu button
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>
+{
+     navbar.classList.toggle('active');
+     searchForm.classList.remove('active');
+     shoppingCart.classList.remove('active');
+     loginForm.classList.remove('active');
+}
+
+// this is code to show only one button at a time
+window.onscroll = () =>
+{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+}
+
+
